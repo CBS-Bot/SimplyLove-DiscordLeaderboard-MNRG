f168afaa2a813a939db88879e06885eeff7f0bcf
Axel Sparkster <axelsparkster@gmail.com>
2025-03-16 15:41:42 -0500

    quick hotfix

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -1232,11 +1232,14 @@ def send_message():
                     
                     embed.add_field(name="Top Server Scores", value=top_scores_message, inline=False)
 
-                    if(data.get('pack').contains('ITL Online 2025')):
-                        print(f"Pack was ITL Online 2025.")
-                    else:
-                        print(f"Pack was not an ITL pack.")
-                    
+                    try:
+                        if(data.get('pack').contains('ITL Online 2025')):
+                            print(f"Pack was ITL Online 2025.")
+                        else:
+                            print(f"Pack was not an ITL pack.")
+                    except Exception as e:
+                        print(f"Error occurred, continuing.")
+
                     asyncio.run_coroutine_threadsafe(channel.send(embed=embed, file=file, allowed_mentions=discord.AllowedMentions.none()), client.loop)
                 #else:
                     #print(f"Channel with ID {channel_id} not found in guild {guild.name}")
